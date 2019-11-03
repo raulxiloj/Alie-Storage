ac45e1889379da5ebff01b7e044868e90151348c
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-11-03 16:03:06 -0600

    Informacion de la base de datos

---
+++ server/src/keys.ts
@@ -0,0 +1,11 @@
+export default{
+    database:{
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: 'Proyecto2',
+        poolMin: 10,
+        poolMax: 10,
+        poolIncrement: 0
+    }
+}
