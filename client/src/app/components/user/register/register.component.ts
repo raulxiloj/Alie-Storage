b07fd3c28e011b536750def6032748a0b2fe23ab
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-11-09 20:42:46 -0600

    Componentes userRegister (formulario de registro) listo

--- client/src/app/components/user/register/register.component.ts
+++ client/src/app/components/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { DataApiService } from 'src/app/services/data-api.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = {
+    username: '',
+    nombre: '',
+    apellido: '',
+    clave: '',
+    correo: '',
+    telefono: 0,
+    fotografia: '',
+    genero: '',
+    fecha_nacimiento: '',
+    direccion : '',
+    estado: 0
+  }
+
+  constructor(private dataApiService: DataApiService, private router:Router) { }
 
   ngOnInit() {
+
+  }
+
+  registerUser(){
+    this.user.clave = this.genericPassword();
+    this.dataApiService.registerUser(this.user)
+    .subscribe(
+      res => {
+        this.router.navigate(['/user/confirmation']);
+      },
+      err => console.error(err)
+    )
+  }
+
+  genericPassword(): string{
+    return Math.random().toString(36).slice(-8);
   }
 
 }
