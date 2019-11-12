4d661ef388e91de4b8f2f5b776cdb81f7ab9f682
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-11-11 23:07:53 -0600

    -Diseno del componente login - listo -Login funcionando

--- client/src/app/components/user/login/login.component.ts
+++ client/src/app/components/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataApiService } from 'src/app/services/data-api.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user:any ={};
+
+  constructor(private dataApi: DataApiService, private router: Router, private location: Location) { }
 
   ngOnInit() {
+
   }
+
+  login(){
+    this.dataApi.signIn(this.user)
+    .subscribe(
+      res => {
+        this.router.navigate(['/user/Home']);
+        this.location.replaceState("/user/Home");
+        location.reload();
+      }
+    )
+  }
+
+
+
+  
 
 }
