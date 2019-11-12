52a1ad2e4dd5b58d4afe0e2685d8676b62e5cffe
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-11-11 23:20:16 -0600

    Se crearon las siguientes rutas: - / - /user/login - /user/register - /user/profile - /user/confirmation - /user/Home - /admin/editHome - ** (page404) Y se agrego el guard en algunas de ellas

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,51 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { Page404Component } from './components/page404/page404.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { UserHomeComponent } from './components/user/user-home/user-home.component';
+import { AuthGuard } from './guards/auth.guard';
+import { EditHomeComponent } from './components/admin/edit-home/edit-home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'user/login',
+    component: LoginComponent
+  },
+  {
+    path: 'user/register',
+    component: RegisterComponent
+  },
+  {
+    path: 'user/profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'user/confirmation',
+    component: ConfirmationComponent
+  },
+  {
+    path: 'user/Home',
+    component: UserHomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'admin/editHome',
+    component: EditHomeComponent
+  },
+  {
+    path: '**',
+    component: Page404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
