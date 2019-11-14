ecfbd756b6cef99fdce64a65b1033334b9e39931
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-11-13 21:17:02 -0600

    Se modificaron las tablas de la base de datos

--- database/database.sql
+++ database/database.sql
@@ -1,32 +1,19 @@
 CREATE TABLE usuario(
     id INTEGER PRIMARY KEY,
-    username VARCHAR2(40) NOT NULL,
-    nombre1 VARCHAR2(40),
-    nombre2 VARCHAR2(40),
-    apellido1 VARCHAR2(40),
-    apellido2 VARCHAR2(40),
+    username VARCHAR2(40) UNIQUE NOT NULL,
     clave VARCHAR2(30) NOT NULL,
+    nombre VARCHAR2(40),
+    apellido VARCHAR2(40),
     correo VARCHAR2(60),
     telefono INTEGER,
     fotografia VARCHAR2(255),
     genero VARCHAR2(1),
     fecha_nacimiento DATE,
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    direccion VARCHAR2(200)    
+    direccion VARCHAR2(200),
+    estado NUMBER(1,0),
+    tipo INTEGER
 );
-
-INSERT INTO usuario (username,clave) VALUES('raxl','123');
-
-CREATE SEQUENCE user_seq START WITH 1;
-
-CREATE TRIGGER user_trigger
-BEFORE INSERT ON usuario
-FOR EACH ROW
-BEGIN 
-    SELECT user_seq.NEXTVAL
-    INTO :new.id
-    FROM dual;
-END;
 
 CREATE TABLE disco(
     id INTEGER PRIMARY KEY,
@@ -39,20 +26,26 @@ CREATE TABLE particion(
     id INTEGER PRIMARY KEY,
     nombre VARCHAR2(50),
     tamano INTEGER,
-    tipo_sistema INTEGER,
     id_disco INTEGER,
     id_usuario INTEGER,
     FOREIGN KEY (id_Disco) REFERENCES disco(id),
     FOREIGN KEY (id_Usuario) REFERENCES usuario(id)
 );
 
+CREATE TABLE sistema_archivos(
+    id INTEGER PRIMARY KEY,
+    tipo INTEGER NOT NULL,
+    id_part INTEGER NOT NULL,
+    FOREIGN KEY (id_part) REFERENCES particion(id)
+);
+
 CREATE TABLE carpeta(
     id INTEGER PRIMARY KEY,
     nombre VARCHAR2(60) NOT NULL,
     padre INTEGER,
-    particion INTEGER,
+    fs INTEGER,
     FOREIGN KEY (padre) REFERENCES carpeta(id),
-    FOREIGN KEY (particion) REFERENCES particion(id)
+    FOREIGN KEY (fs) REFERENCES sistema_archivos(id)
 );
 
 CREATE TABLE archivo(
@@ -62,3 +55,75 @@ CREATE TABLE archivo(
     carpeta INTEGER,
     FOREIGN KEY (carpeta) REFERENCES carpeta(id)
 );
+
+CREATE TABLE homePage(
+    nombre VARCHAR2(50),
+    eslogan VARCHAR2(255),
+    imagen_logo VARCHAR2(300),
+    video VARCHAR2(300),
+    mision VARCHAR2(400),
+    vision VARCHAR2(400),
+    about VARCHAR2(400)
+);
+
+CREATE SEQUENCE user_seq START WITH 1;
+CREATE SEQUENCE disk_seq START WITH 500;
+CREATE SEQUENCE part_seq START WITH 100;
+CREATE SEQUENCE fs_seq   START WITH 1;
+
+CREATE TRIGGER user_trigger
+BEFORE INSERT ON usuario
+FOR EACH ROW
+BEGIN 
+    SELECT user_seq.NEXTVAL
+    INTO :new.id
+    FROM dual;
+END;
+
+CREATE TRIGGER disk_trigger
+BEFORE INSERT ON disco
+FOR EACH ROW
+BEGIN 
+    SELECT disk_seq.NEXTVAL
+    INTO :new.id
+    FROM dual;
+END;
+
+CREATE OR REPLACE TRIGGER  part_trigger
+BEFORE INSERT ON particion
+FOR EACH ROW
+BEGIN 
+    SELECT part_seq.NEXTVAL
+    INTO :new.id
+    FROM dual;
+END;
+
+CREATE OR REPLACE TRIGGER  fs_trigger
+BEFORE INSERT ON sistema_archivos
+FOR EACH ROW
+BEGIN 
+    SELECT fs_seq.NEXTVAL
+    INTO :new.id
+    FROM dual;
+END;
+
+INSERT INTO homePage(nombre,eslogan,mision,vision,about) 
+VALUES('ALIE STORAGE','Need more space? Cloud storage is the solution, is the future','Give you a place to storage your folders, files and more information where you can access anywhere you are.','Continue supporting people around the world with their information, innovating our services daily.','Alie Storage makes it posibble to store practically limitless amount of data in a simple way. It is commonly used for data archiving and backup.');
+
+-- DROP TABLES
+DROP TABLE archivo;
+DROP TABLE carpeta;
+DROP TABLE sistema_archivos;
+DROP TABLE particion;
+DROP TABLE disco;
+DROP TABLE usuario;
+-- DROP SEQUENCES
+DROP SEQUENCE user_seq;
+DROP SEQUENCE disk_seq;
+DROP SEQUENCE part_seq;
+DROP SEQUENCE fs_seq;
+-- DROP TRIGGERS
+DROP TRIGGER user_trigger;
+DROP TRIGGER disk_trigger;
+DROP TRIGGER part_trigger;
+DROP TRIGGER fs_trigger;
