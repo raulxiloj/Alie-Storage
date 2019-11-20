b7caca8308be0b5dff99a18a0eed80ec56de3406
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-11-20 11:07:12 -0600

    Diseno modificado y se renderiza dependiendo de si un usuario esta loggeado o no

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { UserService } from 'src/app/services/user.service';
+import { DataApiService } from 'src/app/services/data-api.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public isLogged: boolean = false;
+  data: any = [];
+  public isUser: boolean= false;
+  public isAdmin: boolean = false;
+
+  constructor(private userService: UserService, private router: Router, private location: Location, private dataApi: DataApiService) { }
 
   ngOnInit() {
+    this.onCheckUser();
+    this.dataApi.getHomeData().subscribe(
+      res => {
+        this.data = res;
+      }
+    )
+  }
+
+  logout(){
+    this.userService.logout();
+    this.router.navigate(['/']);
+    this.location.replaceState("/");
+    location.reload();
+  }
+
+  onCheckUser(): void{
+    if(this.userService.getToken() === null){
+      this.isLogged = false;
+    }else{
+      if(this.userService.getUser() !== 'admin')
+        this.isUser = true;
+      else
+        this.isAdmin = true;
+      
+      this.isLogged = true;
+    }
   }
 
 }
