f3735eaf3915bf16f5faf01b8fe3a0784043d2f6
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-11-20 10:59:04 -0600

    Declaraciones, importaciones, providers, entryComponents(modal)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';       
+import { Page404Component } from './components/page404/page404.component';
+import { DataApiService } from './services/data-api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { ToastrService } from './services/toastr.service';
+import { UserHomeComponent } from './components/user/user-home/user-home.component';
+import { EditHomeComponent } from './components/admin/edit-home/edit-home.component';
+import { FileSystemComponent } from './components/user/file-system/file-system.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTreeModule, MatIconModule, MatButtonModule, MatDialogModule, MatFormFieldModule,MatInputModule } from '@angular/material'
+import { UserService } from './services/user.service';
+import { ModComponent } from './components/modal/mod/mod.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    Page404Component,
+    ConfirmationComponent,
+    UserHomeComponent,
+    EditHomeComponent,
+    FileSystemComponent,
+    ModComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    DataApiService,
+    UserService,
+    ToastrService
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [ModComponent]
 })
 export class AppModule { }
