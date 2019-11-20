1d7fcd17fdf60937e9098726c94d7350e08967fd
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-11-20 11:25:40 -0600

    Servicio para las notificaciones realizado con toastr

--- client/src/app/services/toastr.service.ts
+++ client/src/app/services/toastr.service.ts
@@ -7,10 +7,46 @@ declare var toastr: any;
 })
 export class ToastrService {
 
-  constructor() { }
-
-  Success(title: string, message?:string){
-    toastr.success(title,message);
+  constructor() {
+    this.Settings();
   }
+
+  Success(title: string, message?: string){
+    toastr.success(message,title);
+  }
+
+  Error(title: string, message?: string){
+    toastr.error(message,title);
+  }
+
+  Info(title: string, message?: string){
+    toastr.info(message,title);
+  }
+
+  Warning(title: string, message?: string){
+    toastr.warning(message,title);
+  }
+
+  Settings(){
+    toastr.options = {
+      "closeButton": true,
+      "debug": false,
+      "newestOnTop": true,
+      "progressBar": false,
+      "positionClass": "toast-top-right",
+      "preventDuplicates": false,
+      "onclick": null,
+      "showDuration": "300",
+      "hideDuration": "1000",
+      "timeOut": "5000",
+      "extendedTimeOut": "1000",
+      "showEasing": "swing",
+      "hideEasing": "linear",
+      "showMethod": "fadeIn",
+      "hideMethod": "fadeOut"
+    }
+  }
+
+    
 
 }
