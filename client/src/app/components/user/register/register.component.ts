1816d0ee34dfed0d33d7e917f54e4b81215a9698
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-11-20 11:11:03 -0600

    Componente registro modificado y terminado

--- client/src/app/components/user/register/register.component.ts
+++ client/src/app/components/user/register/register.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { DataApiService } from 'src/app/services/data-api.service';
 import { Router } from '@angular/router'
+import { UserService } from 'src/app/services/user.service';
+
+interface HtmlInputEvent extends Event{
+  target: HTMLInputElement & EventTarget;
+}
 
 @Component({
   selector: 'app-register',
@@ -16,23 +21,26 @@ export class RegisterComponent implements OnInit {
     apellido: '',
     clave: '',
     correo: '',
-    telefono: 0,
-    fotografia: '',
     genero: '',
     fecha_nacimiento: '',
     direccion : '',
     estado: 0
   }
 
-  constructor(private dataApiService: DataApiService, private router:Router) { }
+  file: File;
+  photoSelected: string | ArrayBuffer;
+
+  constructor(private userService: UserService, private router:Router) { }
 
   ngOnInit() {
 
   }
 
   registerUser(){
+    console.log(this.user);
     this.user.clave = this.genericPassword();
-    this.dataApiService.registerUser(this.user)
+    this.user.username = this.user.username.toLowerCase();
+    this.userService.createUser(this.user,this.file)
     .subscribe(
       res => {
         this.router.navigate(['/user/confirmation']);
@@ -43,6 +51,16 @@ export class RegisterComponent implements OnInit {
 
   genericPassword(): string{
     return Math.random().toString(36).slice(-8);
+  }
+
+  onPhotoSelected(event:HtmlInputEvent):void{
+    if(event.target.files && event.target.files[0]){
+      this.file = event.target.files[0];
+      //image preview
+      const reader = new FileReader();
+      reader.onload = e => this.photoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
   }
 
 }
