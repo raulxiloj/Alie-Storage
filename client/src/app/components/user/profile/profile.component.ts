ea53b70b5288250c2d95bc35307fa9c88488b8fa
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-11-20 11:19:42 -0600

    Componente profile -Diseno listo -Renderiza la informacion almacenada del usuario

--- client/src/app/components/user/profile/profile.component.ts
+++ client/src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { ToastrService } from 'src/app/services/toastr.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  userData: any = {}
+  newData: any = {}
+
+  constructor(private userService: UserService, private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.userService.getUserData(this.userService.getUser()).subscribe(
+      res => {
+        this.userData = res;
+      },
+      err => console.log(err)
+    )
+  }
+
+  saveChanges(){
+    if(Object.entries(this.newData).length > 0){
+      this.newData.hasOwnProperty("firstname") ? console.log("new firstname") : this.newData.firstname = this.userData.firstname;
+      this.newData.hasOwnProperty("lastname") ? console.log("new lastname") : this.newData.lastname = this.userData.lastname;
+      this.newData.hasOwnProperty("email") ? console.log("new address") : this.newData.email = this.userData.email;
+      this.newData.hasOwnProperty("phone") ? console.log("new phone number") : this.newData.phone = this.userData.phone;
+      this.newData.hasOwnProperty("address") ? console.log("new address") : this.newData.address = this.userData.address;
+      this.newData.hasOwnProperty("password") ? console.log("new pass"): this.newData.password = this.userData.password;
+      this.newData.username = this.userData.username;
+      this.userService.updateUser(this.newData).subscribe(
+        res => {
+          this.toastr.Success("Data updated succesfully");
+        }
+      );
+    }else{
+      this.newData.firstname = this.userData.firstname;
+      this.newData.lastname = this.userData.lastname;
+      this.newData.email = this.userData.email;
+      this.newData.phone = this.userData.phone;
+      this.newData.address = this.userData.address;
+      this.newData.password = this.userData.password;
+      this.newData.username = this.userData.username;
+      this.userService.updateUser(this.newData).subscribe(
+        res => {
+          this.toastr.Success("Data updated succesfully");
+        }
+      );
+    }
+  }
+
+  showPassword(){
+    var x = <HTMLInputElement>document.getElementById("myPassword");
+    if(x.type === 'password')
+      x.type = 'text';
+    else
+      x.type = 'password';
   }
 
 }
